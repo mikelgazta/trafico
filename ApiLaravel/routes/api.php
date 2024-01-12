e2036969d2a0a3fca182cf0d5bccdcbae2d755e0
Mikel <mikelgaztargmail.com>
2024-01-12 16:41:06 +0100

    He creado los ficheros de laravel a mano porque no me funcionaba el docker con php asique igual hay que reiniciar el proyecto

--- ApiLaravel/routes/api.php
+++ ApiLaravel/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,26 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::middleware('auth:api')->group(function () {
+    // Tus rutas protegidas aquí
+
+    
+    // Ejemplo: Obtener información del usuario autenticado
+    Route::get('/user', function () {
+        return auth()->user();
+    });
+
+    // Ejemplo: Logout (revocar token de acceso)
+    Route::post('/logout', 'Auth\LoginController@logout');
+});
+
+// Rutas públicas que no requieren autenticación
+
+// Ejemplo: Registro de usuario
+Route::post('/register', 'Auth\RegisterController@register');
+
+// Ejemplo: Inicio de sesión
+Route::post('/login', 'Auth\LoginController@login');
